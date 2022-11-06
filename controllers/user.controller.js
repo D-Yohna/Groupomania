ee8877c86f005b72df9f8b5319058419df4c1d81
D-Yohna <anthony.duplan6326@gmail.com>
2022-11-06 18:15:07 +0100

    Crud User

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,36 +11,90 @@ module.exports.getUserInfo = async (req, res) => {
         { return res.status(400).send("ID unknown :" + req.params.id) }
     
     userModel.findById(req.params.id, (err, docs) => {
-        if(!err) res.send(docs)
-        else res.status(400).send(err)
+        if(!err) return res.send(docs)
+        else return res.status(400).send(err)
     }).select("-password");
-    return;
+    
 }
 
 module.exports.updateBio = async (req, res) => {
     if (!ObjectID.isValid(req.params.id))
         { return res.status(400).send("ID unknown :" + req.params.id) }
-
     try {
         await userModel.findOneAndUpdate(
             {
                 _id : req.params.id
             },
             {
-                $set : {
-                    bio : req.body.bio
-                }
+                bio : req.body.bio
             },
             {
                 new: true, 
                 upsert: true, 
                 setDefaultsOnInsert: true
-            },
-            (err, docs) => {
-                if(!err) {
-                    return res.send(docs)
-                }
-            }
-        )
-    } catch(err) {return res.status(500).json({message : err})}; 
-}; 
+            })
+            .select("-password")
+            .then((docs) => res.send(docs))
+            .catch((err) => res.status(500).send({ message: err }))
+    } catch(err) {console.log(err)}
+};
+
+module.exports.deleteUser = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        { return res.status(400).send("ID unknown :" + req.params.id) }
+    
+    try { 
+        await userModel.remove({_id : req.params.id}).exec();
+        return res.status(200).send("User successfully deleted")
+    } catch(err) {return res.status(500).json(err)}
+}
+
+module.exports.follow = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) return res.status(400).send("ID unknown :" + req.params.id);
+    else if (!ObjectID.isValid(req.body.idToFollow)) return res.status(400).send("ID unknown :" + req.body.idToFollow)
+    else {
+        try {
+            //Add to the following list
+            await userModel.findByIdAndUpdate(
+                {_id : req.params.id},
+                {$addToSet : {followings : req.body.idToFollow}},
+                {new: true, upsert: true}
+            )
+            .then((docs) => res.send(docs))
+            .catch((err) =>  res.status(400).send({ message: err }));
+
+            //Add to the followers list
+            await userModel.findByIdAndUpdate(
+                {_id : req.body.idToFollow},
+                {$addToSet : {followers : req.params.id}},
+                {new: true, upsert: true}
+            )
+            .catch((err) => res.status(400).send({ message: err }));
+        } catch(err) {return res.status(500).send({message : err})}
+    }
+}
+
+module.exports.unfollow = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) return res.status(400).send("ID unknown :" + req.params.id);
+    else if (!ObjectID.isValid(req.body.idToUnfollow)) return res.status(400).send("ID unknown :" + req.body.idToUnfollow)
+    else {
+        try {
+            //Remove to the following list
+            await userModel.findByIdAndUpdate(
+                {_id : req.params.id},
+                {$pull : {followings : req.body.idToUnfollow}},
+                {new: true, upsert: true}
+            )
+            .then((docs) => res.send(docs))
+            .catch((err) =>  res.status(400).send({ message: err }));
+
+            //Remove to the followers list
+            await userModel.findByIdAndUpdate(
+                {_id : req.body.idToUnfollow},
+                {$pull : {followers : req.params.id}},
+                {new: true, upsert: true}
+            )
+            .catch((err) => res.status(400).send({ message : err }));
+        } catch(err) {return res.status(500).send({message : err})}
+    }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,7 +5,10 @@ const userController = require('../controllers/user.controller.js');
 router.post("/register", authController.signUp);
 
 router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserInfo)
-router.put("/:id", userController.updateBio)
+router.get("/:id", userController.getUserInfo);
+router.put("/:id", userController.updateBio);
+router.delete("/:id", userController.deleteUser);
+router.patch("/follow/:id", userController.follow);
+router.patch("/unfollow/:id", userController.unfollow);
 
 module.exports = router;
